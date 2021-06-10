c01ceb51095a72799c1164b16439079dcf0334b1
Eddie Naff <eddie@eddienaff.dev>
2021-06-10 14:43:38 -0700

    Refactor to control the number of iterations

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,10 @@
 package iteration
 
-const repeatCount = 5
+// const repeatCount = 5
 
-func Repeat(character string) string {
+func Repeat(character string, iterations int) string {
 	var repeated string
-	for index := 0; index < repeatCount; index++ {
+	for index := 0; index < iterations; index++ {
 		repeated += character
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,29 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T){
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
+var testConstant = "b"
+
+func ExampleRepeat(){
+	repeated := Repeat(testConstant,6)
+	fmt.Println(repeated)
+	// Output: bbbbbb
+}
+
 func BenchmarkRepeat(b *testing.B){
 	for index := 0; index < b.N; index++ {
-		Repeat("a")
+		Repeat("a", 8)
 	}
 }
