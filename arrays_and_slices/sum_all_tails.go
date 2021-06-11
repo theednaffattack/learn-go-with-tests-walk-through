8e0dc2d105b8c4deabfb7e938d1822aad5462889
Eddie Naff <eddie@eddienaff.dev>
2021-06-10 19:17:10 -0700

    Refactor to accept empty slices

--- arrays_and_slices/sum_all_tails.go
+++ arrays_and_slices/sum_all_tails.go
@@ -1,19 +1,20 @@
 package main
 
 func SumAllTails(numbersToSum ...[]int) []int {
-	// Initialize a slice
+	// Create a slice
 	var sums []int
 
 	// Loop over the supplied slice using "range"
 	for _, numbers := range numbersToSum {
-		// Slice the slice to grab the tail.
-		// This should grab all but the first
-		// member of the slice.
-		tail := numbers[1:]
-		// Append values to "sums". Appended values are
-		// what was in sums (nothing) and the sum of 
-		// the slice tail.
-		sums = append(sums, Sum(tail))
+		// If there is no tail, append zero
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			// Else create the tail and append its
+			// sum.
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
