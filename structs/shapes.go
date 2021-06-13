7604413e5e93cf8035e65d28b51aa8cc512cf24e
Eddie Naff <eddie@eddienaff.dev>
2021-06-13 16:11:22 -0700

    Refactor to use table based tests

--- structs/shapes.go
+++ structs/shapes.go
@@ -6,11 +6,22 @@ type Shape interface {
 	Area() float64
 }
 
-type Rectangle struct {
-	Width float64
+type Triangle struct {
+	Base   float64
 	Height float64
 }
 
+// method for Triangle type
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// method for Rectangle type
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
@@ -19,6 +30,7 @@ type Circle struct {
 	Radius float64
 }
 
+// method to calculate area for Circle type
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
@@ -27,6 +39,6 @@ func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
 }
 
-func Perimeter(rectangle Rectangle) float64  {
+func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
